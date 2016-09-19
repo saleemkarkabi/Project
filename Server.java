30e8d35bb035ea2ddfe9960147a3ccaa4a5a25fb
RhombusRobbie <robert.jaroslav.graham@gmail.com>
2016-09-19 15:27:56 -0400

    Formatting
    Added comments, changed all "....." to "...", added appropriate whitespace. Up to line 140.
    
    Variables "file" and "mode" must be longer than one byte to be able to contain the file name and transfer mode.
    Must check for terminating 0 byte in request.

--- Server.java
+++ Server.java
@@ -5,7 +5,7 @@
  * 100898624
  * September 2016
  * 
- * Server Class
+ * Receives a request from an intermediate host and parses the request for valid format. If the packet is invalid, throws an exception and quits. If the packet is a read or write request sends back 0301 or 0400 respectively.
  */
 import java.io.IOException;
 import java.net.*;
@@ -22,8 +22,9 @@ public class Server
 	{
 		try
 		{
+			// Datagram socket bound to port 69 on the local host machine
+			// This socket will be used to receive UDP Datagram packets
 			receiveSocket = new DatagramSocket(69);	
-			// DatagramSocket to use to receive(port 69)
 			
 		}
 		catch(SocketException se)
@@ -32,25 +33,29 @@ public class Server
 			System.exit(1);
 		}
 	}
+	
 	/**
-	 * Algorithm for Server
+	 * Waits for 5 seconds to receive a packet, parses received data for validity, then prints received info. Constructs a send packet and sends it back from where it came from.
 	 */
 	public void serverAlgorithm()
 	{
 		while(true)
 		{
+			// Byte array for received datagram packet
 			byte[] msg = new byte[50];
+			// Response data
 			byte[] data = new byte[4];
-			
-			receivePacket = new DatagramPacket(msg,msg.length);
+			// Packet in which the request will be stored
+			receivePacket = new DatagramPacket(msg, msg.length);
 			System.out.println("Server is waiting for a packet");
 			
 			try
 			{
-				System.out.println("Waiting.....\n");
+				System.out.println("Waiting...");
 				
 				try
 				{
+					// Wait for 5 seconds
 					Thread.sleep(5000);
 				}
 				
@@ -59,8 +64,10 @@ public class Server
 					e.printStackTrace();
 					System.exit(1);
 				}
+				// Store request data in receive packet
 				receiveSocket.receive(receivePacket);
 			}
+			
 			catch(IOException e)
 			{
 				System.out.println("Error\n" + e);
@@ -70,20 +77,24 @@ public class Server
 		
 			System.out.println("Server has received a packet");
 			
+			// Type of request received
 			String request = "";
 			
 			try
 			{
+				// Invalid opcode
 				if(msg[1] == 0)
 				{
 					throw new NoSuchFieldException();
 				}
+				// Read request
 				if (msg[1] == 1)
 				{
 					request = "Read";
 					data[1] = 3;
 					data[3] = 1;
 				}
+				// Write request
 				if(msg[1] == 2)
 				{
 					request = "Write";
@@ -93,15 +104,21 @@ public class Server
 			
 			catch(NoSuchFieldException e)
 			{
-				System.out.println("Invalid Request..... Quitting");
+				System.out.println("Invalid request... Quitting");
 				System.exit(1);
 			}
 			
-			//Parsing the packet received
-			System.out.println(request + " Request received");
+			// Print type of request received
+			System.out.println(request + " request received");
+			
+			// Parse received packet for validity
+			
+			// File name
 			byte[] file = new byte[1];
+			// Transfer mode
 			byte[] mode = new byte[1];
 			byte[] msgBytes = new byte[1];
+			// Counts amount of zero bytes so far
 			int count = 0;
 			for(int i = 2; i < msg.length; i++)
 			{				
@@ -110,16 +127,19 @@ public class Server
 					count++;
 					if (count == 1)
 					{
+						// Copy requested file name
 						file = Arrays.copyOfRange(msg, 2, i);
 					}
 					if(count == 2)
 					{
+						// Copy requested transfer mode
 						mode = Arrays.copyOfRange(msg, 3 + file.length, i);
 						break;
 					}
 				}	
 			}
-			//Printing the information of the received packet
+			
+			// Print information of received packet
 			String fileName = new String(file);
 			System.out.println("File Name: " + fileName);
 			
@@ -129,30 +149,32 @@ public class Server
 			int len = file.length + mode.length + 4;
 			System.out.println("Length: " + len);
 			
-			String infoString = new String(msg,0,fileName.length() + mode2.length() + 4);
+			String infoString = new String(msg, 0, fileName.length() + mode2.length() + 4);
 			System.out.println("Information as String: " + infoString);
 
 			msgBytes = Arrays.copyOfRange(msg, 0, len);
 			System.out.println("Information as Bytes: "+ Arrays.toString(msgBytes) + "\n");
 			
 			
-			sendPacket = new DatagramPacket(data,data.length,
-					receivePacket.getAddress(),receivePacket.getPort());
+			sendPacket = new DatagramPacket(data,data.length, receivePacket.getAddress(),receivePacket.getPort());
 			
-			//Printing out the information of the packet being sent
+			// Printing out the information of the packet being sent
 			System.out.println("Server sent packet");
 			System.out.println("To Host: " + sendPacket.getAddress());
 			System.out.println("Destination host port: " + sendPacket.getPort());
 			System.out.print("Response Packet: ");
 			
-			for(int k = 0; k<data.length;k++)
+			for(int k = 0; k < data.length; k++)
 			{
 				System.out.print(" " + data[k]);
 			}
 			
 			System.out.println("\n");
 			
-			try{
+			try
+			{
+				// Datagram socket bound to any available port on the local host machine.
+				// This socket will be used to send UDP Datagram packets
 				sendSocket = new DatagramSocket();
 			}
 			catch(SocketException se)
@@ -161,7 +183,8 @@ public class Server
 				System.exit(1);
 			}
 			
-		    try {
+		    try
+		    {
 		        sendSocket.send(sendPacket);
 		    }
 		    catch(IOException e)

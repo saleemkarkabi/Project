43d47becebfb17abf2764aa4a6abd871f231efd7
AlexHoecht <alex.hoecht@hotmail.com>
2016-10-13 15:41:51 -0400

    Write function works now

--- SubServer.java
+++ SubServer.java
@@ -1,128 +1,124 @@
-	import java.io.BufferedWriter;
-	import java.io.File;
-	import java.io.FileWriter;
-	import java.io.IOException;
-	import java.net.*;
-	import java.util.Arrays;
+import java.io.*;
+import java.net.*;
+import java.util.Arrays;
+
 public class SubServer implements Runnable {
-	private DatagramSocket sendSocket;
-	private DatagramPacket sendDataPacket, sendPacket;
-	byte[] data = new byte[516];
-	byte[] ack = new byte[4];
-	private File serverDir;
+	
+////////////////////////////////////////////////////////////////////////////////////////
+/*
+ * IMPORTANT!!!!!!!
+ * SET THE FILE PATH TO THE SERVER DIRECTORY!!!!!!!!!!!!!!!!!!!!!
+ * IF COPYING PATH FROM 'FILE EXPLORER': -ALL '\' MUST BE CHANGED TO '/'
+ *									     -THE PATH SHOULD ALSO END WITH A '/'
+ */
+	private static String filePath = "M:/workspace/SYSC3303_CLIENTSERVER/Server Directory/";
+////////////////////////////////////////////////////////////////////////////////////////
+	
+	private DatagramSocket subServerSocket;
+	private DatagramPacket receivePacket, sendPacket;
+	
+	// Size of the Packet being sent back to the client
+	byte[] sendPacketSize;
+	byte[] receivePacketSize;
+	
+	private byte[] data = new byte[516];
+	private byte[] ack = new byte[4];
+	private byte[] opNum = new byte[4];
+	
+	// Integer representation of the packet number
+	private int packetCounter = 0;
+	
+	// The file being accessed by the client request
 	private File receivedFile;
 	private String fileName;
 	
-	public SubServer ( int target, byte[] d, String fN){
+	public SubServer ( int target, byte[] d, String fN, byte[] requestOp)
+	{
+		// Save the name of the requested file
 		fileName = fN;
+		
+		// Create Socket
 		try
 		{
-			sendSocket = new DatagramSocket();	
+			subServerSocket = new DatagramSocket();	
 		}
 		catch(SocketException se)
 		{
 			se.printStackTrace();
 			System.exit(1);
 		}
-		try {
-			sendPacket = new DatagramPacket (ack, ack.length-1,InetAddress.getLocalHost(), target);
-		} catch (UnknownHostException e) {
+		
+		// Create packet that will be sent back to the client
+		// If we received a Read request, we will send DATA packets and receive ACK packets
+		if(requestOp[1] == 3)
+		{
+			sendPacketSize = new byte[516];
+			receivePacketSize = new byte[4];
+		}
+		// If we received a Write request, we will send ACK packets and receive DATA packets
+		else
+		{
+			sendPacketSize = new byte[4];
+			receivePacketSize = new byte[516];
+		}
+		
+		// Creating Datagram Packets!
+		try 
+		{
+			// Create the packet of designated size, and points to the address and port of
+			// the client
+			sendPacket = new DatagramPacket (sendPacketSize, sendPacketSize.length-1,
+													InetAddress.getLocalHost(), target);
+		} 
+		catch (UnknownHostException e) 
+		{
 			e.printStackTrace();
 		}
-			try {
-				sendDataPacket = new DatagramPacket (d, d.length, InetAddress.getLocalHost(), target);
-			} catch (UnknownHostException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+
+		// Create the packet of designated size, and points to the address and port of
+		// the client
+		receivePacket = new DatagramPacket (receivePacketSize, receivePacketSize.length);
+		
 		data = d;
-		serverDir = new File("Server Directory");
-		// If the directory doesnt already exist, create it
-		if(!serverDir.exists())
-		{
-			try
-			{
-			//If the directory is successfully created
-				serverDir.mkdir();
-			}
-			catch(SecurityException se)
-			{
-				System.exit(1);
-			}
-		}
+		ack = requestOp;
 	}
 		
-		
 	
-
 	@Override
-	public void run() {
-		System.out.println("data " + data[1]);
-		if (data[1] == 2){
-			sendAck(2, true);
+	public void run() 
+	{
+		//System.out.println("data " + data[1]);
+		
+		// If write request
+		if (data[1] == 2)
+		{
+			// File creation
 			System.out.println(fileName + " is being created");
-			receivedFile = new File(serverDir,fileName);
+			fileCreation(fileName);
+			
+			sendAck(2, true);
 			waitForData();
 		}
-		sendSocket.close();		
-	}
-	
-	public void waitForData(){
-		System.out.println("Server is waiting for data packet");
-		receive();
-		while(sendDataPacket.getData()[515] != 0x00){
-			handleWrite(sendDataPacket.getData());
-			System.out.println("data packet has been writen to file");
-			receive();
+		// If read request
+		else if(data[1] == 1)
+		{
+			// HANDLE READ REQUEST STUFFFFF
 		}
-		handleWrite(sendDataPacket.getData());
-		System.out.println("final data packet has been writen to file");
-	}
-	
-	public void receive(){
-		try {
-			sendSocket.receive(sendDataPacket);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		else
+		{
+			// ERROR REQUEST REACHED
 		}
+		subServerSocket.close();		
 	}
 	
-	public void sendPacket(){
-		try {
-			sendSocket.send(sendDataPacket);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
 	
-	public void appendToFile(File f, byte[] byteData)
-    {
-    	String filePath = "C:/Users/alexh/Desktop/school/Carleton/Year 4/Sysc 3303/GroupProject/";
-    	try
-    	{
-    		String stringData = new String(byteData);
-
-    		File file = new File(filePath + f);
-    		System.out.println(file.getAbsolutePath().toString());
-    		FileWriter fw = new FileWriter(file.getAbsolutePath(),true);
-    		BufferedWriter bw = new BufferedWriter(fw);
-    		bw.write(stringData);
-    		bw.close();
-    	}
-    	catch (IOException e)
-    	{
-    		e.printStackTrace();
-    	}
-    	}
-	
-	public void fileCreation(byte[] f){
+	public void fileCreation(String f)
+	{
 		// Convert the received file name back into a string
-    	fileName = new String(f);
+    	//String filePath = "C:/Users/alexh/Desktop/school/Carleton/Year 4/Sysc 3303/GroupProject/";
     	
     	// Create the file to be added to the directory
-    	File receivedFile = new File(serverDir,fileName);
+    	receivedFile = new File(f);
     	
     	if(!receivedFile.exists())
     	{
@@ -145,36 +141,167 @@ public class SubServer implements Runnable {
     		}
     	}
 	}
+	
+	
+	public void appendToFile(File f, byte[] byteData)
+    {
+    	try
+    	{
+    		String stringData = new String(cutEnd(byteData));
+
+    		File file = new File(filePath + f);
+    		System.out.println(file.getAbsolutePath().toString());
+    		
+    		FileWriter fw = new FileWriter(file.getAbsolutePath(),true);
+    		BufferedWriter bw = new BufferedWriter(fw);
+    		bw.write(stringData);
+    		bw.close();
+    	}
+    	catch (IOException e)
+    	{
+    		e.printStackTrace();
+    	}
+    }
+	
+	
+	public void sendAck (int code, boolean start)
+	{
+		//Printing out the information of the packet being sent
+		System.out.println("Server sent ACK packet");
+		System.out.println("To Host: " + sendPacket.getAddress());
+		System.out.println("Destination host port: " + sendPacket.getPort());
+		System.out.print("Packet: ");
+		
+		for(int k = 0; k<ack.length;k++)
+		{
+			System.out.print(" " + ack[k]);
+		}
+		// sendPacket data is set to the ACK packet
+		sendPacket.setData(ack);
+		
+		System.out.println();
+		try
+		{
+			subServerSocket.send(sendPacket);
+		} 
+		
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+	
+	
+	public void sendAck (byte[] code)
+	{
+		sendPacket.setData(code);
+		System.out.println("Server sent ACK packet");
+		System.out.println("To Host: " + sendPacket.getAddress());
+		System.out.println("Destination host port: " + sendPacket.getPort());
+		System.out.print("Response Packet: ");
+		
+		for(int k = 0; k<ack.length;k++)
+		{
+			System.out.print(" " + ack[k]);
+		}
+		
+		System.out.println();
+		
+		try 
+		{
+			subServerSocket.send(sendPacket);
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+	
+	
+	public void waitForData()
+	{
+		System.out.println("Server is waiting to receive a data packet");
+		
+		receive();
+		
+		while(receivePacket.getData()[515] != (byte) 0)
+		{
+			handleWrite(receivePacket.getData());
+			System.out.println("data packet has been writen to file");
+			receive();
+		}
+		
+		handleWrite(receivePacket.getData());
+		System.out.println("final data packet has been writen to file");
+		// Reset the packet counter
+		packetCounter = 0;
+	}
+	
+	
+	public void receive(){
+		try 
+		{
+			subServerSocket.receive(receivePacket);
+		} 
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+			System.exit(0);
+		}
+	}
+	
+	
+	public void sendPacket(){
+		try {
+			subServerSocket.send(receivePacket);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	
 	public void handleWrite(byte[] f)
     {
-    	
-    	byte[] opNum = new byte[4];
+    	packetCounter++;
     	System.arraycopy(f, 0, opNum, 0, 4);
-
-    	appendToFile(receivedFile, sendDataPacket.getData());
+    	opNum[3] = (byte) packetCounter;
+    	byte[] writeData = resize(receivePacket.getData());
+    	appendToFile(receivedFile, writeData);
     	sendAck(opNum);
-    	sendPacket();
-
-	
     }
-	public void sendAck (int code, boolean start){
-		sendPacket.getData()[1] = 0x04;
-		System.out.println("Sending ACK to client " + sendPacket.getPort());
-		try {
-			sendSocket.send(sendPacket);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	public void sendAck (byte[] code){
-		sendPacket.setData(code);
-		try {
-			sendSocket.send(sendPacket);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+	
+	
+	// resizes arrays to remove null at the end
+    public byte[] resize (byte[] data)
+    {
+    	byte[] temp = new byte[data.length];
+    	int j = 0;
+    	for(int i = 4; i < data.length; i++)
+    	{
+    		temp[j] = data[i];
+    		j++;
+    	}
+    	return temp;
+    }
+    public byte[] cutEnd (byte[] data)
+    {
+    	int i;
+    	for(i = 4; i < data.length; i++)
+	{
+    		if(data[i] == 0x00)
+		{
+    			break;
+    			
+    		}
+    	}
+    	
+    	data = Arrays.copyOf(data, i+1);
+    	
+    	return data;
+    }
+    
 
 }
